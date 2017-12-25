a44557e735284ee47d02ff38659685d154cf2057
kasulani <kasulani@gmail.com>
2017-12-26 01:35:03 +0300

    adds api endpoints

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')
+    ),
+    re_path('(?P<version>(v1|v2))/', include('api.urls', namespace='shop_list_api')),
 ]
