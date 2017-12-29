ceafffa836a17a4dbaaf068108272f8b3df64dd6
kasulani <kasulani@gmail.com>
2017-12-29 22:40:03 +0300

    re-factors urls as a result of changing to APIViews from Viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,35 +1,15 @@
-from django.urls import re_path, include
-from .views import UserProfileView
-# from rest_framework.routers import DefaultRouter
-
-
-# router = DefaultRouter()
-# router.register(r'users', UserProfileView)
+from django.urls import re_path
+from .views import (
+    ListAllUsers,
+    RegisterUser,
+    SingleUserDetail
+)
+from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'shop_list_api'
 
-# I am binding views-sets explicitly to urls instead of using
-# the router class because the endpoints have specific urls
-
-# views to manipulate a single user profile
-user = UserProfileView.as_view(actions={
-    'put': 'update',
-    'get': 'retrieve',
-    'delete': 'destroy'
-})
-
-# views to manipulate all users
-all_users = UserProfileView.as_view(actions={
-    'get': 'list'
-})
-
-# views to register a single user
-register_user = UserProfileView.as_view(actions={
-    'post': 'create'
-})
-
-urlpatterns = [
-    re_path('^auth/register/$', register_user, name='shop-list-api-register-user'),
-    re_path('^users/$', all_users, name='shop-list-api-all-users'),
-    re_path('^users/(?P<username>[\w.@+-]+)/$', user, name='shop-list-api-user'),
-]
+urlpatterns = format_suffix_patterns([
+    re_path('^auth/register/$', RegisterUser.as_view(), name='shop-list-api-register-user'),
+    re_path('^users/$', ListAllUsers.as_view(), name='shop-list-api-all-users'),
+    re_path('^users/(?P<username>[\w.@+-]+)/$', SingleUserDetail.as_view(), name='shop-list-api-user'),
+])
