39315ba9438cf8a962d74f9d1ce400136fb33f0c
kasulani <kasulani@gmail.com>
2017-12-29 22:38:27 +0300

    re-factors UserProfile mode tests as a result of removing the token field

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -9,20 +9,3 @@ class UserProfileTest(AuthBaseTest):
     def test_user_profile(self):
         # test if the profile created in the setup exists
         self.assertEqual(self.user_profile.description, 'i am a test user')
-
-    def test_generate_auth_token(self):
-        # test if an auth token is generated
-        self.assertIsNotNone(self.token)
-        self.assertEqual(self.user_profile.token, self.token)
-
-    def test_decode_auth_token(self):
-        # test if a valid auth token can be decoded
-        decoded = self.user_profile.decode_auth_token(self.token)
-        self.assertTrue(decoded['status'])
-        self.assertEqual(decoded['details'], 'test_user')
-
-    def test_decode_invalid_auth_token(self):
-        # test if an invalid token is decoded
-        decoded = self.user_profile.decode_auth_token("EUnjqiwe.iSNAmks.opajsd")
-        self.assertFalse(decoded['status'])
-        self.assertEqual(decoded['details'], 'Invalid token. Please register or login')
