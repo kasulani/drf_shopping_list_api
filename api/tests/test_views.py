a62d95db665de1293f768c1a8bc69ae965944fc0
kasulani <kasulani@gmail.com>
2017-12-29 22:39:26 +0300

    re-factors tests as a result of changes in base test

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import status
 from api.tests.base import AuthBaseTest
 from django.urls import reverse
+import json
 
 
 class UserProfileViewTest(AuthBaseTest):
@@ -14,9 +15,12 @@ class UserProfileViewTest(AuthBaseTest):
             'shop_list_api:shop-list-api-all-users',
             kwargs={'version': 'v1'}
         )
+        self.login_client()
         response = self.client.get(url)
         serialized = self.get_all_expected_user_profiles()
+        # assert data is as expected
         self.assertEqual(response.data, serialized.data)
+        # assert status code
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_a_single_user_profile(self):
@@ -29,9 +33,16 @@ class UserProfileViewTest(AuthBaseTest):
                 'username': 'test_user'
             }
         )
+        self.login_client()
         response = self.client.get(url)
         serialized = self.get_expected_single_user_profile()
-        self.assertEqual(response.data, serialized.data)
+        # assert that the data is as expected
+        self.assertEqual(response.data['first_name'], serialized.data['first_name'])
+        self.assertEqual(response.data['last_name'], serialized.data['last_name'])
+        self.assertEqual(response.data['email'], serialized.data['email'])
+        self.assertEqual(response.data['description'], serialized.data['description'])
+        self.assertEqual(response.data['date_joined'], serialized.data['date_joined'])
+        # assert status code
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_a_single_non_existing_user_profile(self):
@@ -44,7 +55,9 @@ class UserProfileViewTest(AuthBaseTest):
                 'username': 'testuser'
             }
         )
+        self.login_client()
         response = self.client.get(url)
+        # assert status code
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_create_a_user_profile_with_valid_data(self):
@@ -57,22 +70,24 @@ class UserProfileViewTest(AuthBaseTest):
         )
         response = self.client.post(
             url,
-            data=self.valid_data,
+            data=json.dumps(self.valid_data),
             content_type='application/json'
         )
+        # assert status code is 201 CREATED
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_a_user_profile_with_invalid_data(self):
-            # test creating a user with invalid data
-            url = reverse(
-                'shop_list_api:shop-list-api-register-user',
-                kwargs={
-                    'version': 'v1'
-                }
-            )
-            response = self.client.post(
-                url,
-                data=self.invalid_data,
-                content_type='application/json'
-            )
-            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # test creating a user with invalid data
+        url = reverse(
+            'shop_list_api:shop-list-api-register-user',
+            kwargs={
+                'version': 'v1'
+            }
+        )
+        response = self.client.post(
+            url,
+            data=json.dumps(self.invalid_data),
+            content_type='application/json'
+        )
+        # assert status code
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
