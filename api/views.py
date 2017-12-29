606f4881ad0818dff36f204767b39b5268a70e15
kasulani <kasulani@gmail.com>
2017-12-29 22:41:54 +0300

    re-factors views from a single Viewset to APIViews

--- api/views.py
+++ api/views.py
@@ -1,13 +1,18 @@
-from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.views import status
+from rest_framework.views import APIView, status
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAdminUser,
+    AllowAny
+)
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from django.contrib.auth.models import User
 from api.serializers import (
-    UserProfileSerializer,
-    UserSerializer,
+    # UserProfileSerializer,
+    # UserSerializer,
     CompositeUserSerializer
 )
 from api.models import UserProfile
-from django.contrib.auth.models import User
 from api.utils import (
     fetch_all_user_profiles,
     fetch_single_user,
@@ -15,45 +20,51 @@ from api.utils import (
 )
 
 
-# Create your views here.
+# API endpoint views
 
-
-class UserProfileView(viewsets.ViewSet):
+class ListAllUsers(APIView):
     """
-    ViewSet for the User Profile. A user profile is a
-    combination of data from two models, i.e, the User
-    model from django auth and UserProfile model
+    View to list all users in the system
+
+    * Requires token authentication
+    * Only admin users are able to access this view
     """
+    authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (IsAdminUser,)
 
-    queryset = UserProfile.objects.all()
-
-    @staticmethod
-    def list(request, version):
+    def get(self, request, version, format=None):
         """
-        This view function GETs all user profiles
-        :param request:
-        :param version:
-        :return:
+        Return a list of all users.
         """
-
-        # get all user profiles
         serializer = CompositeUserSerializer(fetch_all_user_profiles(), many=True)
         return Response(serializer.data)
 
-    @staticmethod
-    def create(request, version):
+
+class RegisterUser(APIView):
+    """
+    View to create a user in the system
+
+    * this is a public view
+    * non authenticated users can access it
+    """
+    permission_classes = (AllowAny,)
+
+    def post(self, request, version, format='json'):
         """
+        Create a new user
+
         This view function creates/POSTs a new user. A user data is
         stored in two models, i.e, User and UserProfile.
         Therefore the process of creating a user involves two
         steps;
         step 1: create a user account in the django auth User model
         step 2: create a user profile in the UserProfile model
+
         :param request:
         :param version:
+        :param format:
         :return:
         """
-
         # compose the data
         data = {
             'username': request.data.get('username', None),
@@ -77,28 +88,58 @@ class UserProfileView(viewsets.ViewSet):
             )
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    @staticmethod
-    def retrieve(request, version, username):
+
+class SingleUserDetail(APIView):
+    """
+    Retrieve, update or delete a user instance.
+
+    * Requires token authentication
+    * Only owner of account can view own details
+    * admin users can view details of other users
+    """
+    authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, version, username, format=None):
         """
-        This view function GETs data of a single user
-        as a user profile
-        :param request: request object
-        :param version: api version number
-        :param username: username
-        :return: serialized user profile
+        Retrieve a single user specified by username
+
+        :param request:
+        :param version:
+        :param username:
+        :param format:
+        :return:
         """
 
-        data = fetch_single_user(username=username)
-        if data is not None:
-            serializer = CompositeUserSerializer(data=data)
-            serializer.is_valid()
-            return Response(data=serializer.data)
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        user = None
+        try:
+            if request.user == User.objects.get(username=username):
+                """
+                this branch will be executed if the request user matches
+                the User object that matches the username parameter
+                """
+                user = request.user.username
+            else:
+                """
+                this branch will be executed when the request user does not
+                match the username parameter. The user requesting to see
+                details of another user should be an admin user
+                """
+                if request.user.is_superuser:
+                    user = username
 
-    def update(self, request, version, pk=None):
-        # update a single user profile
-        return Response({})
+            if user is not None:
+                """
+                if the user is not None, go ahead attempt to fetch details for the
+                user and respond appropriately
+                """
+                data = fetch_single_user(username=user)
+                if data is not None:
+                    serializer = CompositeUserSerializer(data=data)
+                    serializer.is_valid()
+                    return Response(data=serializer.data)
+                # return Response(status=status.HTTP_404_NOT_FOUND)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def destroy(self, request, version, pk=None):
-        # delete a single user profile
-        return Response({})
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
