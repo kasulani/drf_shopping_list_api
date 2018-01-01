827fe2283464b9d3066dca5faf1b3c2db89baa93
kasulani <kasulani@gmail.com>
2018-01-01 12:43:17 +0300

    re-factors view urls

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,15 @@
 from django.urls import re_path
 from .views import (
     ListAllUsers,
-    RegisterUser,
-    SingleUserDetail
+    RegisterUsers,
+    SingleUserDetails
 )
 from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'shop_list_api'
 
 urlpatterns = format_suffix_patterns([
-    re_path('^auth/register/$', RegisterUser.as_view(), name='shop-list-api-register-user'),
+    re_path('^auth/register/$', RegisterUsers.as_view(), name='shop-list-api-register-user'),
     re_path('^users/$', ListAllUsers.as_view(), name='shop-list-api-all-users'),
-    re_path('^users/(?P<username>[\w.@+-]+)/$', SingleUserDetail.as_view(), name='shop-list-api-user'),
+    re_path('^users/$', SingleUserDetails.as_view(), name='shop-list-api-user'),
 ])
