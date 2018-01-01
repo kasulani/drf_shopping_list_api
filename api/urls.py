69c6228a08e75f8d8757f840c80f02c8219ab0d1
kasulani <kasulani@gmail.com>
2018-01-01 13:05:11 +0300

    adds urls to the ManageAPIUsers view

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import re_path
+from django.urls import re_path, include
 from .views import (
     ListAllUsers,
     RegisterUsers,
-    SingleUserDetails
+    SingleUserDetails,
+    ManageAPIUsers
 )
 from rest_framework.urlpatterns import format_suffix_patterns
 
@@ -12,4 +13,5 @@ urlpatterns = format_suffix_patterns([
     re_path('^auth/register/$', RegisterUsers.as_view(), name='shop-list-api-register-user'),
     re_path('^users/$', ListAllUsers.as_view(), name='shop-list-api-all-users'),
     re_path('^users/$', SingleUserDetails.as_view(), name='shop-list-api-user'),
+    re_path('^users/(?P<username>[\w.@+-]+)/$', ManageAPIUsers.as_view(), name='shop-list-manage-user'),
 ])
